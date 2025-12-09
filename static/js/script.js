4f871c8fe36d59711067570072f38810a6cac5ec
Rafael Horwood sanchez <Rafaelshs@MacBook-Pro-3.lan>
2025-12-09 12:17:33 +0000

    add cleaner css, media queries

---
+++ static/js/script.js
@@ -0,0 +1,5 @@
+// Adds functionality to mobile
+document.getElementById("nav-toggle").addEventListener("click", function () {
+    document.getElementById("nav-menu").classList.toggle("nav-open");
+    this.classList.toggle("open");
+});
