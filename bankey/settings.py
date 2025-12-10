bb139aa5e4e89f75eb72000707133dd7d2e21192
Rafael Horwood sanchez <Rafaelshs@MacBook-Pro-3.local>
2025-12-10 14:30:03 +0000

    add secret key config

--- bankey/settings.py
+++ bankey/settings.py
@@ -19,10 +19,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-CHANGE-ME"
+# SECRET_KEY = "django-insecure-CHANGE-ME"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 SITE_ID = 1
 AUTH_USER_MODEL = "bankey_account.User"
 
@@ -108,6 +108,7 @@ DATABASES = {
     "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
